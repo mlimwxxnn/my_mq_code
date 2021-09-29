401b6f235ee75b339f4da54864b54d65ad843e9a
zhou <1776387333@qq.com>
2021-09-29 17:26:24 +0800

    试试

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -13,15 +13,18 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class DefaultMessageQueueImpl extends MessageQueue {
 
-    public static final File DISC_ROOT = new File("/essd");
-    public static final File PMEM_ROOT = new File("/pmem");
+    public static final File DISC_ROOT = new File("./essd");
+    public static final File PMEM_ROOT = new File("./pmem");
     public static final File dataFile = new File(DISC_ROOT, "data");
-    public static FileChannel dataChannel;
-
+    public static FileChannel dataWChannel;
+    public static FileChannel dataRChannel;
+    public static final Object lockObj = new Object();
     static {
         try {
-            dataChannel = FileChannel.open(new File(DISC_ROOT, "dataFile").toPath(),
-                    StandardOpenOption.APPEND, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+            if (!dataFile.exists()) dataFile.createNewFile();
+            dataWChannel = FileChannel.open(dataFile.toPath(),
+                    StandardOpenOption.APPEND, StandardOpenOption.WRITE);
+            dataRChannel = FileChannel.open(dataFile.toPath(), StandardOpenOption.READ);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -31,27 +34,45 @@ public class DefaultMessageQueueImpl extends MessageQueue {
     ConcurrentHashMap<String, Byte> topicNameToTopicId = new ConcurrentHashMap<>();
     public ByteBuffer buffer = ByteBuffer.allocate(1024 * 1024);
     // topicId, queueId, dataPosition
-    ConcurrentHashMap<Byte, ConcurrentHashMap<Integer, ArrayList<long[]>>> metaInfo = new ConcurrentHashMap<>();
+    public static ConcurrentHashMap<Byte, ConcurrentHashMap<Integer, ArrayList<long[]>>> metaInfo = new ConcurrentHashMap<>();
+    public static final Unsafe unsafe = UnsafeUtil.unsafe;
+    public static HashSet<Thread> threadSet = new HashSet<>(100);  // 存储调用过append方法的线程
+    public static AtomicInteger blockedTheadCount = new AtomicInteger();
+    public static long forcedDataPosition = 0;
+    public static final long TIMEOUT = 30*60;  // seconds
 
+    public static void killSelf(long timeout) {
+        new Thread(()->{
+            try {
+                Thread.sleep(timeout * 1000);
+                System.out.println("kill myself");
+                System.exit(-1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 
     public DefaultMessageQueueImpl() {
+        killSelf(TIMEOUT);
+
         long dataFilesize;
 
         // 如果数据文件里有东西就从文件恢复索引
         if ((dataFilesize = dataFile.length()) > 0) {
             try {
-                ByteBuffer readBuffer = ByteBuffer.allocate(8);
-                FileChannel channel = FileChannel.open(dataFile.toPath(), StandardOpenOption.READ);
-                channel.read(readBuffer);
-                readBuffer.flip();
+                ByteBuffer readBuffer = ByteBuffer.allocate(7);
+                FileChannel channel = dataRChannel;
                 byte topicId;
                 int queueId;
                 short dataLen = 0;
                 ConcurrentHashMap<Integer, ArrayList<long[]>> topicInfo;
                 ArrayList<long[]> queueInfo;
-                while (channel.position() < dataFilesize) {
+                while (channel.position() + dataLen < dataFilesize) {
                     channel.position(channel.position() + dataLen); // 跳过数据部分只读取数据头部的索引信息
+                    readBuffer.clear();
                     channel.read(readBuffer);
+
                     readBuffer.flip();
                     topicId = readBuffer.get();
                     queueId = readBuffer.getInt();
@@ -66,7 +87,7 @@ public class DefaultMessageQueueImpl extends MessageQueue {
                         queueInfo = new ArrayList<>(64);
                         topicInfo.put(queueId, queueInfo);
                     }
-                    queueInfo.add(new long[]{channel.position(), dataLen});
+                    queueInfo.add(new long[]{channel.position() - 7, dataLen + 7});
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -76,27 +97,53 @@ public class DefaultMessageQueueImpl extends MessageQueue {
 
     @Override
     public long append(String topic, int queueId, ByteBuffer data) {
-        long offset;
         try {
-            File topicFolder = new File(DISC_ROOT, topic);
+            long offset;
+            long pos;
+            threadSet.add(Thread.currentThread());
             byte topicId = getTopicId(topic);
 
             // 根据topicId获取topic下的队列信息
             ConcurrentHashMap<Integer, ArrayList<long[]>> topicInfo = metaInfo.get(topicId);
-            if(topicInfo == null) {
-                topicInfo = new ConcurrentHashMap<Integer, ArrayList<long[]>>();
+            if (topicInfo == null) {
+                topicInfo = new ConcurrentHashMap<>();
                 metaInfo.put(topicId, topicInfo);
             }
             // 根据queueId获取队列在文件中的位置信息
             ArrayList<long[]> queueInfo = topicInfo.get(queueId);
-            if(queueInfo == null){
+            if (queueInfo == null) {
                 queueInfo = new ArrayList<>();
                 topicInfo.put(queueId, queueInfo);
             }
             offset = queueInfo.size();
 
+            ByteBuffer outBuffer = ByteBuffer.allocate(8);
+            outBuffer.put(topicId);
+            outBuffer.putInt(queueId);
+            outBuffer.putShort((short)data.remaining());
+            outBuffer.flip();
 
 
+            synchronized (lockObj) {
+                dataWChannel.write(outBuffer);
+                dataWChannel.write(data);
+                pos = dataWChannel.position();
+            }
+
+            if(blockedTheadCount.get() < 10) {
+                blockedTheadCount.getAndIncrement();
+                unsafe.park(true, 500);
+            }
+            if(pos > forcedDataPosition) {
+                synchronized(lockObj){
+                    if(pos > forcedDataPosition) {
+                        dataWChannel.force(true);
+                        forcedDataPosition = pos;
+                    }
+                }
+            }
+
+            queueInfo.add(new long[]{pos - data.limit() - 7, data.limit() + 7}); // todo: 占用大小待优化
             return offset;
         } catch (Exception e) {
             e.printStackTrace();
@@ -106,68 +153,74 @@ public class DefaultMessageQueueImpl extends MessageQueue {
 
     /**
      * 创建或并返回topic对应的topicId
+     *
      * @param topic 话题名
      * @return
      * @throws IOException
      */
-    private Byte getTopicId(String topic) throws IOException {
+    private Byte getTopicId(String topic) {
 
         Byte topicId = topicNameToTopicId.get(topic);
         if (topicId == null) {
             File topicIdFile = new File(DISC_ROOT, topic);
-            if (!topicIdFile.exists()) {
-                // 文件不存在，这是一个新的Topic，保存topic名称到topicId的映射，文件名为topic，内容为id
-                topicNameToTopicId.put(topic, topicId = (byte) topicCount.getAndDecrement());
-                FileOutputStream fos = new FileOutputStream(new File(DISC_ROOT, topic));
-                fos.write(topicId);
-                fos.flush();
-                fos.close();
-            } else {
-                // 文件存在，topic不在内存，从文件恢复
-                FileInputStream fis = new FileInputStream(new File(DISC_ROOT, topic));
-                topicId = (byte) fis.read();
-            }
+            try {
+                if (!topicIdFile.exists()) {
+                    // 文件不存在，这是一个新的Topic，保存topic名称到topicId的映射，文件名为topic，内容为id
+                    topicNameToTopicId.put(topic, topicId = (byte) topicCount.getAndIncrement());
+                    FileOutputStream fos = new FileOutputStream(new File(DISC_ROOT, topic));
+                    fos.write(topicId);
+                    fos.flush();
+                    fos.close();
+
+                } else {
+                    // 文件存在，topic不在内存，从文件恢复
+                    FileInputStream fis = new FileInputStream(new File(DISC_ROOT, topic));
+                    topicId = (byte) fis.read();
+                }
+            }catch(Exception ignored) {}
         }
         return topicId;
     }
 
     @Override
     public Map<Integer, ByteBuffer> getRange(String topic, int queueId, long offset, int fetchNum) {
-        ArrayList<long[]> position = metaInfo.get(topic).get(queueId);
+        Byte topicId = getTopicId(topic);
+        ArrayList<long[]> queueInfo = metaInfo.get(topicId).get(queueId);
         ByteBuffer readerBuffer = ByteBuffer.allocateDirect(17 * 1024);
         HashMap<Integer, ByteBuffer> ret = new HashMap<>();
-        synchronized (position) {
-            for (int i = (int) offset; i < (int) (offset + fetchNum) && i < position.size(); ++i) {
-                long[] p = position.get(i);
-                ByteBuffer buf = ByteBuffer.allocate((int) p[1]);
-
-                ret.put(i, buf);
+        try {
+            synchronized (queueInfo) {
+                for (int i = (int) offset; i < (int) (offset + fetchNum) && i < queueInfo.size(); ++i) {
+                    long[] p = queueInfo.get(i);
+                    ByteBuffer buf = ByteBuffer.allocate((int) p[1] - 7);
+                    dataRChannel.read(buf, p[0] + 7);
+                    buf.flip();
+                    ret.put(i, buf);
+                }
             }
-        }
+        }catch (Exception ignored){ }
         return ret;
     }
 
-    private <K, V> V getOrPutDefault(Map<K, V> map, K key, V defaultValue) {
-        V retObj = map.get(key);
-        if (retObj != null) {
-            return retObj;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        DefaultMessageQueueImpl inst = new DefaultMessageQueueImpl();
+        ByteBuffer[] buffers = new ByteBuffer[100];
+        for (byte i = 0; i < buffers.length; i++) {
+            buffers[i] = ByteBuffer.allocate(4);
+            buffers[i].put(i);
+            buffers[i].flip();
         }
 
-        map.put(key, defaultValue);
-        return defaultValue;
-    }
+        long offset;
+        offset = inst.append("abc", 1000, buffers[77]);
+        offset = inst.append("abc", 1001, buffers[67]);
+        offset = inst.append("abc", 1001, buffers[42]);
+        offset = inst.append("abc", 1001, buffers[33]);
+        offset = inst.append("abd", 1001, buffers[11]);
 
-    public static void main(String[] args) throws IOException {
-        FileChannel open = FileChannel.open(new File("aaa.txt").toPath(), StandardOpenOption.APPEND, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-        FileChannel channel = open;
-        ByteBuffer buf = ByteBuffer.allocate(128);
-        buf.put("hello world".getBytes());
-        buf.flip();
-        int write = channel.write(buf);
-        buf.position(0);
-        channel.write(buf);
-        channel.force(true);
-        System.out.println(channel.position());
+        Map<Integer, ByteBuffer> ret = inst.getRange("abc", 1001, 0, 100);
+        System.out.println(String.format("Keys: %d", ret.keySet().size()));
 
     }
 
---
+++ src/main/java/io/openmessaging/Main.java
@@ -0,0 +1,12 @@
+package io.openmessaging;
+
+import java.nio.ByteBuffer;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        DefaultMessageQueueImpl inst = new DefaultMessageQueueImpl();
+        Map<Integer, ByteBuffer> ret = inst.getRange("abc", 1001, 0, 100);
+        System.out.println("");
+    }
+}
