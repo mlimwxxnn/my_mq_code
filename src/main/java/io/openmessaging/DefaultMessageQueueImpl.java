90aa3773fe1bc2929b2014d26265f745fffd4780
liang29 <wanglong.email@foxmial.com>
2021-09-30 11:46:58 +0800

    change timeout

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -27,7 +27,7 @@ public class DefaultMessageQueueImpl extends MessageQueue {
     public static FileChannel dataReadChannel;
     public static final AtomicInteger appendCount = new AtomicInteger();
     public static final AtomicInteger getRangeCount = new AtomicInteger();
-    public static final int THREAD_PARK_TIMEOUT = 10;
+    public static final int THREAD_PARK_TIMEOUT = 3;
     public static final int MERGE_MIN_THREAD_COUNT = 20;
     static {
         try {
