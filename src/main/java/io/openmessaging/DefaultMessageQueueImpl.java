0969944e845f3f7eda196c66d0816f67f45a5a45
liang29 <wanglong.email@foxmial.com>
2021-10-01 08:25:49 +0800

    test

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -21,7 +21,7 @@ public class DefaultMessageQueueImpl extends MessageQueue {
     public static File PMEM_ROOT;
     public static final AtomicInteger appendCount = new AtomicInteger();
     public static final AtomicInteger getRangeCount = new AtomicInteger();
-    public static final long KILL_SELF_TIMEOUT = 7 * 60;  // seconds
+    public static final long KILL_SELF_TIMEOUT = 8 * 60;  // seconds
     public static final long THREAD_PARK_TIMEOUT = 2;  // ms
     public static AtomicInteger MERGE_MIN_THREAD_COUNT = new AtomicInteger(5);  // 只是起始
     public static final int groupCount = 4;
@@ -342,72 +342,113 @@ public class DefaultMessageQueueImpl extends MessageQueue {
 
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        DefaultMessageQueueImpl inst = new DefaultMessageQueueImpl();
-        final int n = 1000;
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    ByteBuffer[] buffers = new ByteBuffer[100];
-                    for (byte i = 0; i < buffers.length; i++) {
-                        buffers[i] = ByteBuffer.allocate(4);
-                        buffers[i].put(i);
-                        buffers[i].flip();
-                    }
+        final int threadCount = 40;
+        final int topicCountPerThread = 100;
+        final int queueIdCountPerTopic = 1000;
+        final int writeTimesPerQueueId = 30;
+        ByteBuffer[][][] buffers = new ByteBuffer[threadCount][topicCountPerThread][queueIdCountPerTopic];
+        DefaultMessageQueueImpl mq = new DefaultMessageQueueImpl();
 
-                    long offset;
-
-                    for (int i = 0; i < n; i++) {
-                        ByteBuffer byteBuffer = ByteBuffer.allocate(8);
-                        byteBuffer.putLong(12345L);
-                        byteBuffer.flip();
-                        inst.append("abcmnp", 1000, byteBuffer);
-                        Thread.sleep(4);
-                    }
-
-                    offset = inst.append("abc1", 1000, buffers[77]);
-                    Thread.sleep(100);
-                    offset = inst.append("abc1", 1001, buffers[67]);
-                    Thread.sleep(100);
-                    offset = inst.append("abc1", 1001, buffers[42]);
-                    offset = inst.append("abc1", 1001, buffers[33]);
-                    Thread.sleep(100);
-                    offset = inst.append("abd1", 1001, buffers[11]);
-                }catch (Exception e){}
-                Map<Integer, ByteBuffer> ret = inst.getRange("abc1", 1001, 0, 100);
-                System.out.println(String.format("Keys: %d", ret.keySet().size()));
+        // 初始化源 buffer
+        for (int threadIndex = 0; threadIndex < threadCount; threadIndex++) {
+            for (int topicIndex = 0; topicIndex < topicCountPerThread; topicIndex++) {
+                for (int queueIndex = 0; queueIndex < queueIdCountPerTopic; queueIndex++) {
+                    ByteBuffer buffer = ByteBuffer.allocate(8);
+                    buffer.putLong(queueIndex);
+                    buffer.flip();
+                    buffers[threadIndex][topicIndex][queueIndex] = buffer;
+                }
             }
-        }).start();
-
-
-        ByteBuffer[] buffers = new ByteBuffer[100];
-        for (byte i = 0; i < buffers.length; i++) {
-            buffers[i] = ByteBuffer.allocate(4);
-            buffers[i].put(i);
-            buffers[i].flip();
         }
 
-        long offset;
-
-        for (int i = 0; i < n; i++) {
-            ByteBuffer byteBuffer = ByteBuffer.allocate(8);
-            byteBuffer.putLong(12345L);
-            byteBuffer.flip();
-            inst.append("abcdef", 1000, byteBuffer);
-            Thread.sleep(4);
+        Thread[] threads = new Thread[threadCount];
+        for (int i = 0; i < threadCount; i++) {
+            int threadIndex = i;
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int topicIndex = 0; topicIndex < topicCountPerThread; topicIndex++) {
+                        String topic = topicIndex + "";
+                        for (int queueIndex = 0; queueIndex < queueIdCountPerTopic; queueIndex++) {
+                            for (int t = 0; t < writeTimesPerQueueId; t++) {
+                                mq.append(topic, queueIndex, buffers[threadIndex][topicIndex][queueIndex]);
+                            }
+                        }
+                    }
+                }
+            });
+            threads[i].start();
         }
 
-        offset = inst.append("abc", 1000, buffers[77]);
-        Thread.sleep(100);
-        offset = inst.append("abc", 1001, buffers[67]);
-        Thread.sleep(100);
-        offset = inst.append("abc", 1001, buffers[42]);
-        offset = inst.append("abc", 1001, buffers[33]);
-        Thread.sleep(100);
-        offset = inst.append("abd", 1001, buffers[11]);
+        Map<Integer, ByteBuffer> res = mq.getRange("30", 100, 0, 100);
+        System.out.println(res.size());
 
-        Map<Integer, ByteBuffer> ret = inst.getRange("abc", 1001, 0, 100);
-        System.out.println(String.format("Keys: %d", ret.keySet().size()));
+
+//        final int n = 1000;
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    ByteBuffer[] buffers = new ByteBuffer[100];
+//                    for (byte i = 0; i < buffers.length; i++) {
+//                        buffers[i] = ByteBuffer.allocate(4);
+//                        buffers[i].put(i);
+//                        buffers[i].flip();
+//                    }
+//
+//                    long offset;
+//
+//                    for (int i = 0; i < n; i++) {
+//                        ByteBuffer byteBuffer = ByteBuffer.allocate(8);
+//                        byteBuffer.putLong(12345L);
+//                        byteBuffer.flip();
+//                        inst.append("abcmnp", 1000, byteBuffer);
+//                        Thread.sleep(4);
+//                    }
+//
+//                    offset = inst.append("abc1", 1000, buffers[77]);
+//                    Thread.sleep(100);
+//                    offset = inst.append("abc1", 1001, buffers[67]);
+//                    Thread.sleep(100);
+//                    offset = inst.append("abc1", 1001, buffers[42]);
+//                    offset = inst.append("abc1", 1001, buffers[33]);
+//                    Thread.sleep(100);
+//                    offset = inst.append("abd1", 1001, buffers[11]);
+//                }catch (Exception e){}
+//                Map<Integer, ByteBuffer> ret = inst.getRange("abc1", 1001, 0, 100);
+//                System.out.println(String.format("Keys: %d", ret.keySet().size()));
+//            }
+//        }).start();
+//
+//
+//        ByteBuffer[] buffers = new ByteBuffer[100];
+//        for (byte i = 0; i < buffers.length; i++) {
+//            buffers[i] = ByteBuffer.allocate(4);
+//            buffers[i].put(i);
+//            buffers[i].flip();
+//        }
+//
+//        long offset;
+//
+//        for (int i = 0; i < n; i++) {
+//            ByteBuffer byteBuffer = ByteBuffer.allocate(8);
+//            byteBuffer.putLong(12345L);
+//            byteBuffer.flip();
+//            inst.append("abcdef", 1000, byteBuffer);
+//            Thread.sleep(4);
+//        }
+//
+//        offset = inst.append("abc", 1000, buffers[77]);
+//        Thread.sleep(100);
+//        offset = inst.append("abc", 1001, buffers[67]);
+//        Thread.sleep(100);
+//        offset = inst.append("abc", 1001, buffers[42]);
+//        offset = inst.append("abc", 1001, buffers[33]);
+//        Thread.sleep(100);
+//        offset = inst.append("abd", 1001, buffers[11]);
+//
+//        Map<Integer, ByteBuffer> ret = inst.getRange("abc", 1001, 0, 100);
+//        System.out.println(String.format("Keys: %d", ret.keySet().size()));
 
     }
 }
