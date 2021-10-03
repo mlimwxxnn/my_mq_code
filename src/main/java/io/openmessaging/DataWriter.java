cbaaf9cba284202720038a7fb47d49d564822b85
zhou <1776387333@qq.com>
2021-10-03 20:28:14 +0800

    改了一下超时时间

--- src/main/java/io/openmessaging/DataWriter.java
+++ src/main/java/io/openmessaging/DataWriter.java
@@ -38,7 +38,7 @@ public class DataWriter {
                 try {
                     MergedData mergedData = new MergedData(freeMergeBufferQueue.take()); // 这里只要buffer设置得足够多就不会返回null的
                     for (int i = 0; i < 20; i++) {  // todo 随便设置的一个数，后面可以通过一个变量来控制
-                        WrappedData data = wrappedDataQueue.poll(1, TimeUnit.MILLISECONDS);// todo 这里可以用变量来控制
+                        WrappedData data = wrappedDataQueue.poll(3, TimeUnit.MILLISECONDS);// todo 这里可以用变量来控制
                         if (data != null) {
                             mergedData.putData(data);
                         } else {
