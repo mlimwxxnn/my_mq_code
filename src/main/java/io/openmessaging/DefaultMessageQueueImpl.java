89fd87a7f710262b1a73e52bc2d1263a16322425
zhou <1776387333@qq.com>
2021-10-05 12:11:34 +0800

    修复了打印日志的一些错误

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -104,7 +104,7 @@ public class DefaultMessageQueueImpl extends MessageQueue {
         killSelf(KILL_SELF_TIMEOUT);
         powerFailureRecovery(metaInfo);
         initThreadCount = Thread.activeCount();
-        log.info("DefaultMessageQueueImpl 构造完成")
+        log.info("DefaultMessageQueueImpl 构造完成");
     }
 
     public void powerFailureRecovery(ConcurrentHashMap<Byte, HashMap<Short, HashMap<Integer, long[]>>> metaInfo) {
@@ -194,7 +194,7 @@ public class DefaultMessageQueueImpl extends MessageQueue {
         return topicId;
     }
 
-    Object o = new Object();
+    final Object o = new Object();
     Thread theThread = null;
     @Override
     public Map<Integer, ByteBuffer> getRange(String topic, int queueId, long offset, int fetchNum) {
@@ -206,17 +206,19 @@ public class DefaultMessageQueueImpl extends MessageQueue {
             synchronized (o) {
                 if (theThread == null) {
                     theThread = Thread.currentThread();
+                    log.debug("topic,queueId,offset,fetchNum,queueLen");
                 }
             }
         }
+
+
         if(Thread.currentThread() == theThread) {
             try {
-                log.debug("tpc:{},qId:{},off:{},fn:{}, qLen:{}", topic, queueId, offset, fetchNum, metaInfo.get(getTopicId(topic, false)).get(queueId).size());
+                log.debug("{},{},{},{},{}", topic, queueId, offset, fetchNum, metaInfo.get(getTopicId(topic, false)).get((short)queueId).size());
             }catch (Exception e){
                 e.printStackTrace();
             }
         }
-
 
 
         GetRangeTask task = getTask(Thread.currentThread());
@@ -227,6 +229,8 @@ public class DefaultMessageQueueImpl extends MessageQueue {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+
         return task.getResult();
     }
 
