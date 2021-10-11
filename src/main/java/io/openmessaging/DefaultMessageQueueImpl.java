9711152beb67ece9c764c5a4e5bdee8c45fd1171
zhouzelin <1776387333@qq.com>
2021-10-11 13:32:15 +0800

    2K page_size and the first 20GB of data is not stored.

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -33,7 +33,7 @@ public class DefaultMessageQueueImpl extends MessageQueue {
     public static final long WAITE_DATA_TIMEOUT = 1000;  // 微秒
     public static final int WRITE_THREAD_COUNT = 5;
     public static final int READ_THREAD_COUNT = 20;
-    public static final int PMEM_WRITE_THREAD_COUNT = 20;
+    public static final int PMEM_WRITE_THREAD_COUNT = 4;
     public static final int PMEM_PAGE_SIZE = 2 * 1024;
     public static final int PMEM_BLOCK_COUNT = 112;
     public static final long PMEM_HEAP_SIZE = 59 * GB;
