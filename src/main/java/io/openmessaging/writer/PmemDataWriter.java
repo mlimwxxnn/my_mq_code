1ac3466b47c23fe24bd6821f03853dc5665a4bc9
zhouzelin <1776387333@qq.com>
2021-10-11 19:42:38 +0800

    gai hui qu zai kan kan cheng ji

--- src/main/java/io/openmessaging/writer/PmemDataWriter.java
+++ src/main/java/io/openmessaging/writer/PmemDataWriter.java
@@ -16,7 +16,7 @@ import static io.openmessaging.DefaultMessageQueueImpl.*;
 
 public class PmemDataWriter {
     public static MemoryBlock[] memoryBlocks = new MemoryBlock[PMEM_BLOCK_COUNT];
-    private BlockingQueue<PmemPageInfo> freePmemPageQueue = new LinkedBlockingQueue<>();
+    private final BlockingQueue<PmemPageInfo> freePmemPageQueue = new LinkedBlockingQueue<>();
     public BlockingQueue<WrappedData> pmemWrappedDataQueue = new LinkedBlockingQueue<>();
     private final Semaphore freePageCount = new Semaphore(0);
 
@@ -25,15 +25,18 @@ public class PmemDataWriter {
         boolean initialized = Heap.exists(PMEM_ROOT + "/persistent_heap");
 //        Heap h = initialized ? Heap.openHeap(PMEM_ROOT + "/persistent_heap") : Heap.createHeap(PMEM_ROOT + "/persistent_heap", PMEM_HEAP_SIZE);
         Heap h = Heap.createHeap(PMEM_ROOT + "/persistent_heap", PMEM_HEAP_SIZE);
-        MemoryBlock newBlock;
-        for (int i = 0; i < PMEM_BLOCK_COUNT; i++) {  // 创建pmem存储块
-            newBlock = h.allocateMemoryBlock(PMEM_TOTAL_BLOCK_SIZE / PMEM_BLOCK_COUNT, false);
-            PmemDataWriter.memoryBlocks[i] = newBlock;
-            for (int j = 0; j < PMEM_TOTAL_BLOCK_SIZE / PMEM_BLOCK_COUNT / PMEM_PAGE_SIZE; j++) {
-                this.offerFreePage(new PmemPageInfo((byte)i, j)); // 对创建的内存块进行划分
+        new Thread(()-> {
+            MemoryBlock newBlock;
+            for (int i = 0; i < PMEM_BLOCK_COUNT; i++) {  // 创建pmem存储块
+                newBlock = h.allocateMemoryBlock(PMEM_TOTAL_BLOCK_SIZE / PMEM_BLOCK_COUNT, false);
+                PmemDataWriter.memoryBlocks[i] = newBlock;
+                for (int j = 0; j < PMEM_TOTAL_BLOCK_SIZE / PMEM_BLOCK_COUNT / PMEM_PAGE_SIZE; j++) {
+                    this.offerFreePage(new PmemPageInfo((byte) i, j)); // 对创建的内存块进行划分
+                }
             }
-        }
+        }).start();
     }
+
     public PmemDataWriter() {
         initPmem();
         writeDataToPmem();
