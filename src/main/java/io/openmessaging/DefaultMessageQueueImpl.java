c51b0c3de3cab6cc50d4e32dcc92d4c91bd502db
liang29 <wanglong.email@foxmail.com>
2021-10-16 13:43:29 +0800

    test write speed

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -25,9 +25,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public class DefaultMessageQueueImpl extends MessageQueue {
 
-    public static final boolean GET_CACHE_HIT_INFO = true;
-    public static final boolean GET_WRITE_TIME_COST_INFO = true;
-    public static final boolean GET_READ_TIME_COST_INFO = true;
+    public static final boolean GET_CACHE_HIT_INFO = false;
+    public static final boolean GET_WRITE_TIME_COST_INFO = false;
+    public static final boolean GET_READ_TIME_COST_INFO = false;
     public static final int PMEM_BLOCK_GROUP_COUNT = 17;
     public static final Logger log = LoggerFactory.getLogger("myLogger");
     public static final long GB = 1024L * 1024L * 1024L;
