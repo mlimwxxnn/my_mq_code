630b254bb3edc0571abc53a16ddb4b9605203be2
zhou <1776387333@qq.com>
2021-11-01 20:47:37 +0800

    2800M heapBuffer

--- src/test/java/io/openmessaging/Test.java
+++ src/test/java/io/openmessaging/Test.java
@@ -1,11 +1,25 @@
 package io.openmessaging;
 import javax.sound.midi.Soundbank;
 import java.nio.ByteBuffer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static io.openmessaging.DefaultMessageQueueImpl.*;
 public class Test {
     public static void main(String[] args)  {
-
+        System.out.println(Thread.activeCount());
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        System.out.println(Thread.activeCount());
+        for (int i = 0; i < 10; i++) {
+            executor.submit(() -> {
+                System.out.println("thread id is: " + Thread.currentThread().getId());
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
     }
 }
